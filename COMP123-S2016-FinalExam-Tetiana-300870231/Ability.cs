e81836a788ffcec37beeb3b2c6c3477dedd53ce9
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-08-19 10:40:52 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam-Tetiana-300870231/Ability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2016_FinalExam_Tetiana_300870231
+{
+    public enum Ability
+    {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma
+    }
+}
